886392e3fbb1146aa799696eeb3cbb39b4e8984c
michael.rempel@gmx.net <michael.rempel@gmx.net>
2020-02-01 00:51:43 +0100

    implement Detector search function with reflection on SearchAlgo

--- src/Infrastructure/LHC/Detector.java
+++ src/Infrastructure/LHC/Detector.java
@@ -1,6 +1,10 @@
 package Infrastructure.LHC;
 
+import java.io.File;
 import java.io.Reader;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.LinkedList;
 
 public class Detector {
@@ -8,4 +12,38 @@ public class Detector {
     private boolean isActivated;
     private LinkedList<Experiment> experimentList;
     private Reader reader;
+
+    private Class stringMatcherClass;
+    private Object port;
+    private Method searchMethod;
+
+    public Detector() {
+
+        try {
+            URL[] urls = {new File(Configuration.instance.fullPathToJavaArchive).toURI().toURL()};
+            URLClassLoader urlClassLoader = new URLClassLoader(urls, Detector.class.getClassLoader());
+            stringMatcherClass = Class.forName(Configuration.instance.nameOfClass, true, urlClassLoader);
+            port = stringMatcherClass.getMethod("getInstance").invoke(null);
+            port = stringMatcherClass.getDeclaredField("port").get(port);
+            searchMethod = port.getClass().getMethod("search",String.class,String.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void search(Experiment experiment){
+
+    }
+
+    private int matchString(String text, String pattern){
+
+        try {
+            return (int) searchMethod.invoke(port,text,pattern);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return  0;
+    }
+
 }
