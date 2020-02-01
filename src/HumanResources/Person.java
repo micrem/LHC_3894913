8c0d4989a259b47c300c6491b44b5553ecf07d65
michael.rempel@gmx.net <michael.rempel@gmx.net>
2020-02-01 22:28:08 +0100

    add getRandomName()

--- src/HumanResources/Person.java
+++ src/HumanResources/Person.java
@@ -4,9 +4,12 @@ import Infrastructure.Security.Biometrics.Iris;
 import Infrastructure.Security.IDCard.IDCard;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class Person {
     static SecureRandom secureRandom = new SecureRandom();
+    static ArrayList<String> names = new ArrayList<>();
 
     protected static int idIncrement=0;
     int id;
@@ -38,6 +41,26 @@ public abstract class Person {
 
     public String enterPassword(){
         return password;
+    }
+
+    protected static String getRandomName(){
+        if(names.isEmpty()){
+        names.add("Nelchael");
+        names.add("Jeremiel");
+        names.add("Algemos Vibiutus");
+        names.add("Elatutus Corastus");
+        names.add("Juliuverus Aquitanus");
+        names.add("Armaron Boulderhowl");
+        names.add("Jonold Firegaze");
+        names.add("Armaron Boulderhowl");
+        names.add("Hectic Rockfall");
+        names.add("Eoninid Hestaddas");
+        names.add("Targuram Vidake");
+        names.add("Agaresh Nidya");
+        }
+        int randomNameIndex = secureRandom.nextInt(names.size());
+        return names.get(randomNameIndex);
+
     }
 }
 
