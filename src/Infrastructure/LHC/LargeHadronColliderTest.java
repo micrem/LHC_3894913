c2a7afaaa771f2b68c55eeffd7da0bbeecca2895
mic.rempel.19@lehre.mosbach.dhbw.de <mic.rempel.19@lehre.mosbach.dhbw.de>
2020-02-07 19:57:50 +0100

    init unit tests

---
+++ src/Infrastructure/LHC/LargeHadronColliderTest.java
@@ -0,0 +1,13 @@
+package Infrastructure.LHC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LargeHadronColliderTest {
+
+    @Test
+    void getRing() {
+        assertEquals(1,1);
+    }
+}
