ddb496b7867f333f595fa7415bf78c3205d23ef6
michael.rempel@gmx.net <michael.rempel@gmx.net>
2020-02-24 22:42:25 +0100

    add toString for Employee

--- src/HumanResources/Employee.java
+++ src/HumanResources/Employee.java
@@ -9,4 +9,10 @@ public abstract class Employee extends Person {
         super(name);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Employee ID:").append(this.id).append(" name:").append(this.name).append(" hasCard:").append(this.getCard()!=null);
+        return builder.toString();
+    }
 }
